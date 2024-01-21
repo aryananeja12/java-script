6da1c31c6919cc1fcf85e0a86844d6e8e3aec892
aryananeja12 <157296998+aryananeja12@users.noreply.github.com>
2024-01-21 17:03:10 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let score="33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33"=> number
+// "33abc"=>NAN
+// true=>1; ,false=>0
+// null =>0
+
+let isLoggedIn=5
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=>true
+// 0=>false
+//""=>false
+// aryan=>true
