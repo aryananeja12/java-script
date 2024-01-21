7df8993f9cff88ae8bd4de35803b4e6421d75967
aryananeja12 <157296998+aryananeja12@users.noreply.github.com>
2024-01-21 16:37:17 +0000

    data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer verison
+
+console.log(3 +3);
+
+console.log("Aryan")
+
+let name="Aryan"
+let age=20
+let isloggedin=false
+
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null =>standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
