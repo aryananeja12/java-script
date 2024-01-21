19f739dd9c311240aa8107f2f553198ce6bada3d
aryananeja12 <157296998+aryananeja12@users.noreply.github.com>
2024-01-21 16:15:23 +0000

    change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=14553
+let accountEmail="anejaaryan877@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId=2 // not allowed
+
+accountCity="Bangalore"
+accountEmail="HcHc.com"
+accountPassword="221"
+console.log(accountId);
+
+// prefer not to use var
+// because of issue in block scope and function scope
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
