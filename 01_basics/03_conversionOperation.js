cbcdb5929cc40f986e98462717f2eefc7c827652
aryananeja12 <157296998+aryananeja12@users.noreply.github.com>
2024-01-22 16:33:29 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-// let score="33abc"
+ let score="33abc"
 
-// console.log(typeof score);
-// console.log(typeof (score));
+console.log(typeof score);//string
+ console.log(typeof (score));//string
 
-// let valueInNumber=Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
+ let valueInNumber=Number(score)
+console.log(typeof valueInNumber)//number
+console.log(valueInNumber)//NAN
 
 // "33"=> number
 // "33abc"=>NAN
@@ -24,44 +24,44 @@
 
 // *******************Operations**************
 
-// let value=3
-// let negValue= -value
-// console.log(negValue);
+let value=3
+let negValue= -value
+console.log(negValue);//-3
 
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
 // console.log(2**3);
 // console.log(2/3);
-// console.log(3%2);
+ console.log(3%2);//1
 
-let str1="hello"
-let str2="Aryan"
+// let str1="hello"
+// let str2="Aryan"
 
-let str3= str1 +str2
-// console.log(str3);
+// let str3= str1 +str2
+// // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 +"2");
-// console.log("1" +"2");
-// console.log("1" +2 +2);
-// console.log(1 +2 +"2");
+console.log("1" + 2);//12
+console.log(1 +"2");//12
+console.log("1" +"2");12
+console.log("1" +2 +2);122
+console.log(1 +2 +"2");32
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);//1
+console.log(+"");//0
 
-let num1, num2, num3
+// let num1, num2, num3
 
-num1 = num2 =num3 = 2 + 2
+// num1 = num2 =num3 = 2 + 2
 
-let gameCounter =100
-let x= gameCounter++
-console.log(gameCounter,x);
-//If used postfix the increment operator increments and returns the value before incrementing.
+// let gameCounter =100
+// let x= gameCounter++
+// console.log(gameCounter,x);
+// //If used postfix the increment operator increments and returns the value before incrementing.
 
 
-let y= ++gameCounter
-console.log(gameCounter, y);
+// let y= ++gameCounter
+// console.log(gameCounter, y);
 //If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
 
 
