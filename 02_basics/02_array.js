591c019a24bd335e0bb0aa2f734a80b37076538a
aryananeja12 <157296998+aryananeja12@users.noreply.github.com>
2024-01-23 21:29:43 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -18,17 +18,17 @@ const anotherArray =[1, 2, 3, [4, 5, 5], 7, [6, 7, [4, 5]]]
 const realAnotherArray = anotherArray.flat(Infinity) //infinity mein daalna hain kitni depth //saare arrays ko ek mein krta hain
 console.log(realAnotherArray); // 1, 2, 3, 4, 5, 5, 7, 6, 7, 4, 5
 
-
-
 console.log(Array.isArray("aryan")); //array hain ya nhi //false
 console.log(Array.from("aryan")); //convert krdenga array mein
 //[ 'a', 'r', 'y', 'a', 'n' ]
 
-console.log(Array.from({name: "aryan"})); //construct array from array like object
+console.log(Array.from({name: "aryan"})); //construct array from array like object only 
 //jb array nhi bnaa paata to empty array //[]
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3)); //represent new array from set of elements //
+console.log(Array.of(score1, score2, score3)); //represent new array from set of elements // kisi ko bhi array mein krdenga
+
+console.log(Array.of({name: "Aryan"})); //[ { name: 'Aryan' } ]
