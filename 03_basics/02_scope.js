5f49f42d6c60bd8791a555e052e7a9285120665c
aryananeja12 <157296998+aryananeja12@users.noreply.github.com>
2024-01-23 21:29:51 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,13 @@
+let a=300
+
+if(true){
+    let a =10
+const b=20
+//var c=30
+console.log("Inner:",a);
+}
+
+
+console.log(a); //yeh print nhi honga
+//console.log(b); // yeh bhi ni honga
+//console.log(c); // yeh hojenga
